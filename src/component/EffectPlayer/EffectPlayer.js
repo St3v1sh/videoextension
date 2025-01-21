2c341f7484d0c9bce9f4641f16464428378c4a06
st3v1sh <st3v1sh@gmail.com>
2025-01-20 21:05:14 -0500

    use uuid for unique ids, add sync queue checkbox for video playback, sync playback bug fix if multiple videos added at same time

--- src/component/EffectPlayer/EffectPlayer.js
+++ src/component/EffectPlayer/EffectPlayer.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './EffectPlayer.module.css';
 
 const VALID_EXTENSIONS = ['mp4', 'webm', 'ogg'];
@@ -13,8 +13,7 @@ const V_ALIGNMENTS_MAP = {
     'bottom': 'v-bottom',
 };
 
-export default function EffectPlayer({ videoQueue, onEnded }) {
-
+export function EffectPlayer({ videoQueue, onEnded }) {
     useEffect(() => {
         videoQueue.forEach((videoData) => {
             if (!VALID_EXTENSIONS.includes(videoData.fileName.split('.').pop())) {
@@ -34,5 +33,57 @@ export default function EffectPlayer({ videoQueue, onEnded }) {
                 </div>
             ))}
         </>
+    );
+}
+
+export function EffectPlayerSync({ videoQueue }) {
+    const [videoIndex, setVideoIndex] = useState(0);
+    const [videoExtension, setVideoExtension] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const onEnded = () => {
+        if (videoIndex < videoQueue.length - 1) {
+            setVideoIndex((previousIndex) => previousIndex + 1);
+        } else {
+            setIsPlaying(false);
+        }
+    };
+
+    useEffect(() => {
+        if (videoQueue.length > 0 && isPlaying) {
+            const [, extension] = videoQueue[videoIndex].fileName.split('.');
+
+            // Skip invalid files.
+            if (!VALID_EXTENSIONS.includes(extension)) {
+                console.log(`Invalid file extension for video: ${JSON.stringify(videoQueue[videoIndex])}`);
+                onEnded();
+            } else {
+                setVideoExtension(extension);
+                videoRef.current?.load();
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [videoIndex, isPlaying]);
+
+    useEffect(() => {
+        if (videoQueue.length > videoIndex && !isPlaying) {
+            setVideoIndex(videoIndex === 0 ? 0 : videoIndex + 1);
+            setIsPlaying(true);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [videoQueue]);
+
+    const { fileName, xOffset, yOffset, hAlignment, vAlignment } = videoQueue[videoIndex] || {};
+
+    return (
+        <div className={styles.videofx + (videoQueue.length > 0 ? (' ' + styles[H_ALIGNMENTS_MAP[hAlignment]] + ' ' + styles[V_ALIGNMENTS_MAP[vAlignment]]) : '')}>
+            {
+                isPlaying &&
+                <video className={styles['video-player']} style={{ left: `${xOffset}px`, top: `${yOffset}px` }} autoPlay muted ref={videoRef} onEnded={onEnded}>
+                    <source src={`./videos/${fileName}`} type={`video/${videoExtension}`} />
+                </video>
+            }
+        </div>
     );
 }
